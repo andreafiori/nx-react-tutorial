0a50155501b0ec764c73e036762028ef90b3726f
Andrea <a.fiori@cheapnet.it>
2020-03-14 22:24:07 +0100

    Todos API backend fix

--- apps/api/src/app/todos.ts
+++ apps/api/src/app/todos.ts
@@ -6,8 +6,9 @@ const todos: Todo[] = [{ id: 1, title: 'Todo 1' }, { id: 2, title: 'Todo 2' }];
 export function addTodoRoutes(app: Express) {
   app.get('/api/todos', (req, resp) => resp.send(todos));
   app.post('/api/addTodo', (req, resp) => {
+    const newTodoId = Math.floor(Math.random() * 1000);
     const newTodo = {
-      title: `New todo ${Math.floor(Math.random() * 1000)}`
+      id: newTodoId, title: `New todo ${newTodoId}`
     };
     todos.push(newTodo);
     resp.send(newTodo);
